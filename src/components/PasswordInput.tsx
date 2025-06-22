de1c94749c34c4468290b4140c37dec88f59e110
benazeerhassan1909 <91200605+benazeerhassan1909@users.noreply.github.com>
2025-06-22 12:01:09 +0530

    PLugin updates

--- src/components/PasswordInput.tsx
+++ src/components/PasswordInput.tsx
@@ -1,6 +1,5 @@
-import {TextInput} from '@sanity/ui'
-import React from 'react'
-import {useState} from 'react'
+import {Box, Button, Flex, TextInput} from '@sanity/ui'
+import React, {forwardRef, useState} from 'react'
 import {set, unset} from 'sanity'
 
 interface PasswordInputProps {
@@ -8,39 +7,33 @@ interface PasswordInputProps {
   onChange: (patchEvent: ReturnType<typeof set> | ReturnType<typeof unset>) => void
 }
 
-export function PasswordInput({value, onChange}: PasswordInputProps) {
-  const [visible, setVisible] = useState(false)
+export const PasswordInput = forwardRef<HTMLInputElement, PasswordInputProps>(
+  function PasswordInput({value, onChange}, ref) {
+    const [visible, setVisible] = useState(false)
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const inputValue = event.currentTarget.value
-    onChange(inputValue ? set(inputValue) : unset())
-  }
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      const inputValue = event.currentTarget.value
+      onChange(inputValue ? set(inputValue) : unset())
+    }
 
-  const handleToggleVisibility = () => {
-    setVisible((prev) => !prev)
-  }
-
-  return (
-    <div>
-      <TextInput
-        type={visible ? 'text' : 'password'}
-        value={value || ''}
-        onChange={handleInputChange}
-      />
-      <button
-        type="button"
-        onClick={handleToggleVisibility}
-        style={{
-          marginTop: '5px',
-          fontSize: '12px',
-          background: 'transparent',
-          border: 'none',
-          color: '#0070f3',
-          cursor: 'pointer',
-        }}
-      >
-        {visible ? 'Hide' : 'Show'} Password
-      </button>
-    </div>
-  )
-}
+    return (
+      <Box>
+        <Flex align="center" gap={2}>
+          <TextInput
+            ref={ref}
+            type={visible ? 'text' : 'password'}
+            value={value || ''}
+            onChange={handleInputChange}
+            style={{flex: 1}}
+          />
+          <Button
+            mode="bleed"
+            text={visible ? 'Hide' : 'Show'}
+            padding={2}
+            onClick={() => setVisible((v) => !v)}
+          />
+        </Flex>
+      </Box>
+    )
+  },
+)
